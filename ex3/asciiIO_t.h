2e8ab55fd97eecc8651c30a20763d0192c474579
stradivarius10 <pooyae@mail.tau.ac.il>
2015-05-15 12:41:31 +0300

    assio.h

--- ex3/asciiIO_t.h
+++ ex3/asciiIO_t.h
@@ -7,43 +7,219 @@
 class asciiIO_t : public virtIO_t
 {
 public:
-	virtual ~asciiIO_t();
+
+	virtual ~asciiIO_t(){}
 	asciiIO_t(const string &path, const string & mode);
 
-	virtual virtIO_t& operator>>(void* Buf);
-	virtual virtIO_t& operator<<(const void* Buf);
+	// since we are implementing other << we don't want to hide these
+	using virtIO_t::operator<<;
+	using virtIO_t::operator>>;
 
 
-	virtual virtIO_t& operator>>(char) = 0;
-	virtual virtIO_t& operator<<(const char) = 0;
-	virtual virtIO_t& operator>>(unsigned char) = 0;
-	virtual virtIO_t& operator<<(const unsigned char) = 0;
-	virtual virtIO_t& operator>>(short) = 0;
-	virtual virtIO_t& operator<<(const short) = 0;
-	virtual virtIO_t& operator>>(unsigned short) = 0;
-	virtual virtIO_t& operator<<(const unsigned short) = 0;
-	virtual virtIO_t& operator>>(int) = 0;
-	virtual virtIO_t& operator<<(const int) = 0;
-	virtual virtIO_t& operator>>(unsigned int) = 0;
-	virtual virtIO_t& operator<<(const unsigned int) = 0;
-	virtual virtIO_t& operator>>(long) = 0;
-	virtual virtIO_t& operator<<(const long) = 0;
-	virtual virtIO_t& operator>>(unsigned long) = 0;
-	virtual virtIO_t& operator<<(const unsigned long) = 0;
-	virtual virtIO_t& operator>>(float) = 0;
-	virtual virtIO_t& operator<<(const float) = 0;
-	virtual virtIO_t& operator>>(double) = 0;
-	virtual virtIO_t& operator<<(const double) = 0;
+	virtual virtIO_t& operator>>(char &c);
+	virtual virtIO_t& operator<<(char c);
+	virtual virtIO_t& operator>>(unsigned char &c);
+	virtual virtIO_t& operator<<(unsigned char c);
+	virtual virtIO_t& operator>>(short &c);
+	virtual virtIO_t& operator<<(short c);
+	virtual virtIO_t& operator>>(unsigned short &c);
+	virtual virtIO_t& operator<<(unsigned short c);
+	virtual virtIO_t& operator>>(int &c);
+	virtual virtIO_t& operator<<(int c);
+	virtual virtIO_t& operator>>(unsigned int &c);
+	virtual virtIO_t& operator<<(unsigned int c);
+	virtual virtIO_t& operator>>(long &c);
+	virtual virtIO_t& operator<<(long c);
+	virtual virtIO_t& operator>>(unsigned long &c);
+	virtual virtIO_t& operator<<(unsigned long c);
+	virtual virtIO_t& operator>>(float &c);
+	virtual virtIO_t& operator<<(float c);
+	virtual virtIO_t& operator>>(double &c);
+	virtual virtIO_t& operator<<(double c);
 
 
 protected:
-	inline void set_length(size_t len);
+
 
 private:
+	void read_file_with_format(const string& format,  const void * x);
 	asciiIO_t(const asciiIO_t &); //we don't suport copying
 	asciiIO_t& operator=(const asciiIO_t&); //we don't suport copying
+	//change status if it has problems... template so we can get the value witht he right type
+	template <class T> void write_file_with_format(const string& format, const T& value);
+	
 
 };
+
+
+
+
+template <class T> void asciiIO_t::write_file_with_format(const string& format, const T& value)
+{
+
+	int result = fprintf(this->get_file(), format.c_str(), value);
+	if (result < 1)
+	{
+		this->set_status(virtIO_t::writeErr_e);
+	}
+
+	else
+	{
+		this->set_status(virtIO_t::ok_e);
+
+	}
+}
+
+
+// inline implementation
+
+// inline implementation
+
+// Please note: I've asked Yossi nd he told me I don't have to use Templeate functions here though I could..
+inline virtIO_t& asciiIO_t::operator >> (char & c)
+{
+	read_file_with_format("%c", &c);
+	return *this;
+}
+
+inline virtIO_t& asciiIO_t::operator<< (const char c)
+{
+	write_file_with_format("%c", c);
+	return *this;
+}
+
+
+inline virtIO_t& asciiIO_t::operator >> (unsigned char& c)
+{
+	read_file_with_format("%c", &c);
+	return *this;
+}
+
+
+inline virtIO_t& asciiIO_t::operator<< (unsigned char c)
+{
+	write_file_with_format("%c", c);
+	return *this;
+}
+
+
+inline virtIO_t& asciiIO_t::operator >> (short& c)
+{
+	read_file_with_format("%hd", &c);
+	return *this;
+}
+
+
+inline virtIO_t& asciiIO_t::operator<< (short c)
+{
+	write_file_with_format("%hd", c);
+	return *this;
+}
+
+
+inline virtIO_t& asciiIO_t::operator >> (unsigned short& c)
+{
+	read_file_with_format("%c", &c);
+	return *this;
+}
+
+
+inline virtIO_t& asciiIO_t::operator<< (unsigned short c)
+{
+	write_file_with_format("%hu", c);
+	return *this;
+}
+
+
+
+inline virtIO_t& asciiIO_t::operator >> (int& c)
+{
+	read_file_with_format("%d", &c);
+	return *this;
+}
+
+
+inline virtIO_t& asciiIO_t::operator<< (int c)
+{
+	write_file_with_format("%d", c);
+	return *this;
+}
+
+
+inline virtIO_t& asciiIO_t::operator >> (unsigned int& c)
+{
+	read_file_with_format("%u", &c);
+	return *this;
+}
+
+
+inline virtIO_t& asciiIO_t::operator<< (unsigned int c)
+{
+	write_file_with_format("%u", c);
+	return *this;
+}
+
+
+
+inline virtIO_t& asciiIO_t::operator >> (long& c)
+{
+	read_file_with_format("%lu", &c);
+	return *this;
+}
+
+
+inline virtIO_t& asciiIO_t::operator<< (long c)
+{
+	write_file_with_format("%lu", c);
+	return *this;
+}
+
+
+inline virtIO_t& asciiIO_t::operator >> (float& c)
+{
+	read_file_with_format("%f", &c);
+	return *this;
+}
+
+inline virtIO_t& asciiIO_t::operator<< (float c)
+{
+	write_file_with_format("%f", c);
+	return *this;
+}
+
+inline virtIO_t& asciiIO_t::operator >> (double& c)
+{
+	read_file_with_format("%lf", &c);
+	return *this;
+}
+
+
+inline virtIO_t& asciiIO_t::operator<< (double c)
+{
+	write_file_with_format("%lf", c);
+	return *this;
+}
+
+inline virtIO_t& asciiIO_t::operator >> (unsigned long &c)
+{
+	read_file_with_format("%lu", &c);
+	return *this;
+}
+
+
+inline virtIO_t& asciiIO_t::operator<< (unsigned long c)
+{
+	write_file_with_format("%lu", c);
+	return *this;
+}
+
+
+
+
+
+
+
+
 
 
 
