90ccca1faa63558032c29131e221c99a9be6c892
shlomi vaknin <shlomi.39sd@gmail.com>
2015-06-03 16:14:19 +0300

    fix the bug

--- ex5/Observer.cpp
+++ ex5/Observer.cpp
@@ -11,8 +11,15 @@ void Subject::Attach(Observer* ob) {
 void Subject::Detach(Observer* ob) {
 	size_t i = 0;
 	for (i = 0; i < m_observers.size(); i++)
+	{
 		if (m_observers[i] == ob) break;
-	m_observers.erase(m_observers.begin() + i);
+	}
+		
+	if (i != m_observers.size())
+	{
+		m_observers.erase(m_observers.begin() + i);
+	}
+	
 }
 
 void Subject::Notify() {
